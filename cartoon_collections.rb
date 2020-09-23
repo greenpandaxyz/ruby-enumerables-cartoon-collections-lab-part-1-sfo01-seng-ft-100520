ff5b8ae73b9eccba8a121b0715c2c9fea1a6208b
Amanda D <amanda@depaula.xyz>
2020-09-23 20:44:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def greet_characters(array)
-array.each do |element|
-puts "Hello #{element}!"
-end
+  array.each do |element|
+    puts "Hello #{element}!"
+  end
 end
 
 def list_dwarves(array)
   array.each_with_index do |dwarves, index|
-  puts "#{index + 1}. #{dwarves}"
-end
+    puts "#{index + 1}. #{dwarves}"
+  end
 end
